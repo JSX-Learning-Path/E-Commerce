e08077471c03659a0b5ec5c54f0e788008f3fd30
donni88c <cs_neeko@yahoo.com>
2026-05-07 20:19:15 +0300

    feat: Add Google Maps iframe and location details to Footer component

--- commerce/src/Footer.jsx
+++ commerce/src/Footer.jsx
@@ -1,3 +1,5 @@
+import { CiLocationOn } from "react-icons/ci";
+import "./styles/Map.css";
 function Footer() {
   return (
     <div>
@@ -62,6 +64,22 @@ function Footer() {
                 </li>
               </ul>
             </div>
+          </div>
+          <div className=" d-flex flex-column">
+            <iframe
+              title="Our Location"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2959.2857438557094!2d24.728339276752592!3d42.12276567121589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14acd03c0a396b7f%3A0xcdba5a55532fa58!2z0LYu0LouINCl0YDQuNGB0YLQviDQkdC-0YLQtdCyIC0g0K7Qs9Cu0LbQtdC9LCDRg9C7LiDigJ7Qk9C10L7RgNCz0Lgg0JjQutC-0L3QvtC80L7QsuKAnCAxLCA0MDA0INCf0LvQvtCy0LTQuNCy!5e0!3m2!1sbg!2sbg!4v1775375140185!5m2!1sbg!2sbg"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              className=" map-container border-1 rounded shadow-lg hover:width-105% transition-all duration-300 ease-in-out"
+            ></iframe>
+            <p className="bg-dark bg-gradient text-white p-2 d-flex align-items-center gap-1 mt-2 rounded ">
+              <CiLocationOn />1 Georgi Ikonomov St., Plovdiv, Bulgaria
+            </p>
           </div>
         </div>
       </footer>
